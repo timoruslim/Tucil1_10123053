39f50a778995915a18f81a6d15a9168094f5200a
timoruslim <timothyniels@gmail.com>
2025-02-18 20:45:15 +0700

    added - function read .txt input - block data structure - README.md

--- README.md
+++ README.md
@@ -1,18 +1,7 @@
-## Getting Started
+## Tugas Kecil 1 | Strategi Algoritma (IF2211)
 
-Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
+Tugas Kecil 1 untuk mata kuliah Strategi Algoritma (IF2211) berupa IQ Puzzler Pro solver. Permainanan papan IQ Puzzler Pro dengan diberikan papan awal kosong dan beberapa blok diselesaikan dengan algoritma brute force, yaitu dengan mencoba semua permutasi dari balok pada papan hingga mendapatkan solusi (jika ada).
 
-## Folder Structure
+Digunakan bahasa Java. Maka, diperlukan compiler Java dengan JDK (Java Development Kit) perlu terinstall. Untuk memudahkan kompilasi, folder pada repository dapat dibuka menggunakan IDE seperti Visual Studio Code, Netbeans, atau Eclipse sebagai project, lalu file `App.java` dapat di-run.
 
-The workspace contains two folders by default, where:
-
-- `src`: the folder to maintain sources
-- `lib`: the folder to maintain dependencies
-
-Meanwhile, the compiled output files will be generated in the `bin` folder by default.
-
-> If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
-
-## Dependency Management
-
-The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
+Dibuat oleh Timothy Niels Ruslim (10123053).
--- src/App.java
+++ src/App.java
@@ -1,5 +1,70 @@
+import java.io.File;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+class Block extends ArrayList<ArrayList<Character>> {
+    
+    public Block() { 
+        super(); 
+    }
+
+    public void addRow(char[] row) {
+        ArrayList<Character> rowList = new ArrayList<>();
+        for (char letter : row) {
+            rowList.add(letter);
+        }
+        this.add(rowList);
+    }
+
+    public void displayBlock() {
+        for (ArrayList<Character> row : this) {
+            for (Character letter : row) {
+                System.out.print(letter + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
 public class App {
+
+    static void printList(String[] list) {
+        for (String n : list) {
+            System.out.println(n);
+        }
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // Get File
+        Scanner terminalScanner = new Scanner(System.in);
+        // System.out.print("Enter file path: ");
+        // String path = terminalScanner.nextLine( );
+        String path = "../test/Problem_1.txt";
+        File file = new File(path);
+        terminalScanner.close();
+
+        // Read file 
+        Scanner fileScanner = new Scanner(file);
+        String[] dimensions = fileScanner.nextLine().split(" "); 
+        String type = fileScanner.nextLine();
+
+        // Get the Pieces 
+        ArrayList<Block> blocks = new ArrayList<>();
+        Block block = new Block();
+        char[] row = fileScanner.nextLine().toCharArray();
+        block.addRow(row);
+        while (fileScanner.hasNextLine()) {
+            char[] newRow = fileScanner.nextLine().toCharArray();   
+            if (row[0] != newRow[0]) {
+                blocks.add(block);
+                block = new Block();
+                row = newRow;
+            }
+            block.addRow(newRow);
+        }
+        blocks.add(block);
+        fileScanner.close();
+        
     }
 }
