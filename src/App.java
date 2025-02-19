e2fd4873df8099b67d0f6d3e1e036486c55c931a
timoruslim <timothyniels@gmail.com>
2025-02-19 19:55:55 +0700

    added extra block methods and board object - block rotation - block reflection - block padding - board object - board print

--- src/App.java
+++ src/App.java
@@ -14,6 +14,7 @@ class Block extends ArrayList<ArrayList<Character>> {
             rowList.add(letter);
         }
         this.add(rowList);
+        this.padBlock();
     }
 
     public void displayBlock() {
@@ -24,14 +25,111 @@ class Block extends ArrayList<ArrayList<Character>> {
             System.out.println();
         }
     }
+    
+    public Block rotateBlock() { // rotate 90 degrees clockwise
+        Block rotatedBlock = new Block();
+        int rows = width();
+        int cols = this.size();
+
+        for (int i = 0; i < rows; i++) {
+            rotatedBlock.add(new ArrayList<>());
+        }
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
+                char letter = this.get(i).get(j);
+                rotatedBlock.get(j).add(0, letter);  
+            }
+        }
+        
+        return rotatedBlock;
+    }
+
+    public Block reflectBlock() { // reflect about y-axis
+        Block reflectedBlock = new Block();
+        int rows = this.size();
+        
+        for (int i = 0; i < rows; i++) {
+            reflectedBlock.add(new ArrayList<>());
+            for (char letter : this.get(i)) {
+                reflectedBlock.get(i).add(0, letter);  
+            }
+        }
+        
+        return reflectedBlock;
+    }
+
+    public Block padBlock() {
+        Block paddedBlock = new Block();
+        int maxWidth = width();
+        for (ArrayList<Character> row : this) {
+            while (row.size() < maxWidth) {
+                row.add('·');
+            }
+        }
+        return paddedBlock;
+    }
+    
+    public int width() {
+        int maxWidth = 0;
+        for (ArrayList<Character> row : this) {
+            maxWidth = Math.max(maxWidth, row.size());          
+        }
+        return maxWidth;
+    }
+}
+
+class Board extends ArrayList<ArrayList<Character>> {
+
+    public Board(int n, int m) { 
+        super(); 
+
+        for (int i = 0; i < n; i++) {
+            ArrayList<Character> row = new ArrayList<>();
+            for (int j = 0; j < m; j++) {
+                row.add('·');
+            }
+            this.add(row);
+        }
+    }
+
+    public void displayBoard() {
+        for (ArrayList<Character> row : this) {
+            for (char dot : row) {
+                System.out.print(dot);
+            }
+            System.out.println();
+        }
+    }
+
 }
 
 public class App {
 
+    static void print(String string) {
+        System.out.println(string);
+    }
+
+    static void print(int integer) {
+        System.out.println(integer);
+    }
+
+    static void print() {
+        System.out.println();
+    }
+
     static void printList(String[] list) {
         for (String n : list) {
-            System.out.println(n);
+            System.out.print(n + " ");
         }
+        System.out.println();
+    }
+
+    static int[] toInt(String[] list) {
+        int[] intList = new int[list.length];
+        for (int i = 0; i < list.length; i++) {
+            intList[i] = Integer.parseInt(list[i]); 
+        }
+        return intList;
     }
 
     public static void main(String[] args) throws Exception {
@@ -46,8 +144,16 @@ public class App {
 
         // Read file 
         Scanner fileScanner = new Scanner(file);
-        String[] dimensions = fileScanner.nextLine().split(" "); 
+        int[] dimensions = toInt(fileScanner.nextLine().split(" ")); 
         String type = fileScanner.nextLine();
+
+        // Create Board
+        Board board = new Board(dimensions[0], dimensions[1]);
+        if (type == "CUSTOM") {
+            // custom
+        } else if (type == "PYRAMID") {
+            // pyramid
+        }
 
         // Get the Pieces 
         ArrayList<Block> blocks = new ArrayList<>();
@@ -66,5 +172,16 @@ public class App {
         blocks.add(block);
         fileScanner.close();
         
+        for (Block b : blocks) {
+            b.displayBlock();
+            print();
+            b.rotateBlock().displayBlock();
+            print();
+            b.reflectBlock().displayBlock();
+            print();
+        }
+
+        board.displayBoard();
+
     }
 }
